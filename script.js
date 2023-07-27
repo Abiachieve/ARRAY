5a287d0bb0a760bd8afadaf5fd4f7ad320861428
Abiachieve <abinaya.achieve123@gmail.com>
2023-07-27 12:42:40 +0530

    arraynotes

---
+++ script.js
@@ -0,0 +1,38 @@
+// const a = new Date();//date object-a fixed
+// console.log(a.toDateString());
+// const a = new Date();//date object-a fixed
+// console.log(a.getDate());
+// const a = new Date();//date object-a fixed returns month
+// console.log(a.getMonth());
+// const e=new Date();
+// console.log(e.getDay());
+// console.log(e.getHours());
+// console.log(e.getMinutes());
+// console.log(e.getSeconds());
+// console.log(e.getMilliseconds());
+// console.log(e.getTime());
+const f = new Date();
+// f.setDate(27);
+
+//console.log(f);
+// f.setFullYear(2022,5,27);
+// console.log(f);
+
+// f.setHours(8) ;
+// console.log(f);
+// f.setMilliseconds(783) ;
+// console.log(f);
+// f.setMinutes(45) ;
+// console.log(f);
+// f.setSeconds(55) ;
+// console.log(f);
+// f.setMonth(6) ;
+// console.log(f);
+
+// console.log(f.getTime());
+// f.setTime(1658891755783) ;
+// console.log(f);
+
+var a=[8,3,4,a,c]
+console.log(a.sort());
+
